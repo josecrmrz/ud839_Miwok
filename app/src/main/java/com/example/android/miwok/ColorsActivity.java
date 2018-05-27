3bfbc0d62ee6f23d3c7af0fb00e17594cec167cd
Jose Ramirez <josecrmrz@gmail.com>
2018-05-27 00:52:05 -0500

    Use one layout for each category Add English and Miwok translations to each category

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -17,12 +17,31 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kululli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        // Use Custom Array Adapter
+        WordAdapter numbersAdapter = new WordAdapter(this, words);
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(numbersAdapter);
     }
 }
